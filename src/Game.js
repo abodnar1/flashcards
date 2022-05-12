8be5a42c2c571dc45253efbcd3f860e1623356f2
Amber Bodnar <amber.bodnar@gmail.com>
2022-05-12 17:19:23 -0400

    Add game class and test files

--- src/Game.js
+++ src/Game.js
@@ -2,17 +2,35 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+
 class Game {
-  constructor() {}
+  constructor(currentRound) {
+    this.currentRound = currentRound || null;
+  };
+
+  start() {
+    const cards = prototypeQuestions.map(cardInfo => {
+      return new Card(cardInfo.id, cardInfo.question, cardInfo.answers, cardInfo.correctAnswer);
+    });
+
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  };
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
-  }
+  };
 
   printQuestion(round) {
       util.main(round);
-  }
-}
+  ;}
+};
 
 module.exports = Game;
